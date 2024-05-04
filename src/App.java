730b6003a6c818a6b783d6b178c0ae66bb736c10
Edson Sousa <edson3711.es@gmail.com>
2024-05-04 12:46:59 -0300

    cria transações entre duas contas

--- src/App.java
+++ src/App.java
@@ -1,15 +1,28 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Cliente cliente = new Cliente();
-		cliente.setNome("Edson");
-		
-		Conta cc = new ContaCorrente(cliente);
-		Conta poupanca = new ContaPoupanca(cliente);
+	public static void main(String[] args) throws Exception {
+		// cria cliente edson
+		Cliente edson = new Cliente();
+		edson.setNome("Edson");
 
-		cc.depositar(1000.99);
-		cc.transferir(350.55, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-    }
+		Conta cc_edson = new ContaCorrente(edson);
+		Conta poupanca_edson = new ContaPoupanca(edson);
+
+		// cria cliente rosalia
+		Cliente rosalia = new Cliente();
+		rosalia.setNome("Rosalia");
+
+		Conta cc_rosalia = new ContaCorrente(rosalia);
+		Conta poupanca_rosalia = new ContaPoupanca(rosalia);
+
+		// cria transações
+		cc_edson.depositar(1000.99);
+		cc_edson.transferir(350.55, poupanca_rosalia);
+
+		// exibe extratos
+		cc_edson.imprimirExtrato();
+		poupanca_edson.imprimirExtrato();
+		cc_rosalia.imprimirExtrato();
+		poupanca_rosalia.imprimirExtrato();
+
+	}
 }
