6e2b5aa15574463a05ff76a0a625c2b36daf8fd1
Edson Sousa <edson3711.es@gmail.com>
2024-05-04 12:40:01 -0300

    criando classes para testar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente cliente = new Cliente();
+		cliente.setNome("Edson");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		cc.depositar(1000.99);
+		cc.transferir(350.55, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
